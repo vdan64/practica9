27485c7f34da53a13c4b64d0328ddf1e8a88bff9
theloop2 <aguileraa835@gmail.com>
2024-03-04 11:20:50 -0400

    feature bugfix (Update char.)

--- newspapers/urls.py
+++ newspapers/urls.py
@@ -3,7 +3,6 @@ from django.urls import path
 from .views import Updatepost
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('post/<int:pk>/edit/',Updatepost.as_view(),"update_post" ),
 ]
 
--- newspapers/views.py
+++ newspapers/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
-from django.views.generic import UpdateView
+from django.views.generic.edit import UpdateView
 from .models import Post
 # Create your views here.
 class Updatepost (UpdateView):
     model = Post
     template_name = "updatepost.html"
+    fields = ["title", "author", "body"]
