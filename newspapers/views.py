0f3a3451aaeabd4a0ee0937e3e88a808fbe82ea8
CesarCarias <cesarleonardocariasvasquez@gmail.com>
2024-03-04 11:44:17 -0400

    update bug fixes

--- newspapers/views.py
+++ newspapers/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.views.generic import DetailView, ListView
 from .models import Post
 
-# Create your views here.
+# Create your views here.   ``
 class NewsDetailView(DetailView):
     model = Post
     template_name = "postdetail.html"
