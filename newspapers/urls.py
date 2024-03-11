b8c5f7053c388163bdd8516a0fa7f71833ad122f
theloop2 <aguileraa835@gmail.com>
2024-03-11 10:22:11 -0400

    create added

--- newspapers/urls.py
+++ newspapers/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path
-from .views import Updatepost, NewsDetailView, DeletePost
+from .views import Updatepost, NewsDetailView, DeletePost, CreatePost 
 
 urlpatterns = [
     path('post/<int:pk>/edit/', Updatepost.as_view(),name="update_post" ),
     path("post/<int:pk>/", NewsDetailView.as_view(), name="post_detail"),
     path("post/<int:pk>/delete/", DeletePost.as_view(), name="post_delete"),
+    path("post/create/", CreatePost.as_view(), name="post_create"),
 ]
--- newspapers/views.py
+++ newspapers/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import DetailView, ListView
-from django.views.generic.edit import UpdateView, DeleteView
+from django.views.generic.edit import UpdateView, DeleteView, CreateView
 from django.urls import reverse_lazy
 from .models import Post
 
@@ -19,3 +19,8 @@ class DeletePost(DeleteView):
     model = Post
     template_name = "postdelete.html"
     success_url = reverse_lazy("home")
+class CreatePost(CreateView):
+    model = Post
+    template_name = "postcreate.html"
+    fields = ["title","author", "body"]
+
