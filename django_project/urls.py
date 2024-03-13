1537fdef0bbe0d60f7a6868d808884c0f3a422a5
victors <victordaniel64x@gmail.com>
2024-03-13 10:18:45 -0400

    login agregado

--- django_project/urls.py
+++ django_project/urls.py
@@ -23,4 +23,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("newspapers.urls")),
+    path('accounts/', include("django.contrib.auth.urls")),
 ]
